96368e8a92d66f0e32d58b88ae1fe288bf530449
Mark Bogard <87099349+bogeyman76@users.noreply.github.com>
2024-10-21 10:21:00 -0700

    Create documentvisible.js

---
+++ documentvisible.js
@@ -0,0 +1,15 @@
+function DocumentVisible() {
+  const isOnline = useSyncExternalStore(subscribeVisibilityChange, getCurrentVisibilityState);
+  return isOnline === 'visible' ? true : false ;
+}
+
+function getCurrentVisibilityState() {
+  return document.visibilityState;
+}
+
+function subscribeVisibilityChange(callback) {
+  window.addEventListener("visibilitychange", callback);
+  return () => {
+    window.removeEventListener("visibilitychange", callback);
+  };
+}
